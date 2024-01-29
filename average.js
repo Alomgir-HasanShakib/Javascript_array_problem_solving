5960337dcde253b83de62094e0584c73677fc4b1
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2024-01-29 20:48:31 +0600

    solved average problems

--- average.js
+++ average.js
@@ -10,9 +10,11 @@
 // division all the numbers with arrays elements lenght
 function getAverage(numbers) {
   let sum = 0;
+  let average = 0;
   for (let i = 0; i < numbers.length; i++) {
     sum += numbers[i];
+    average = sum / numbers.length;
   }
-  return sum;
+  return average;
 }
 console.log(getAverage([1, 2, 3, 4]));
